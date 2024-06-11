d93f1456040f84cf76524cdc7ee7d10d933882b0
Usama Bhakrani <usamabhakrani@gmail.com>
2024-06-11 09:03:20 +0500

    dynamic route

---
+++ app/api/(dashboard)/categories/[category]/route.ts
@@ -0,0 +1,5 @@
+import { connect } from "@/lib/db";
+import User from "@/lib/models/user";
+import { NextRequest, NextResponse } from "next/server";
+import { Types } from "mongoose";
+import Category from "@/lib/models/category";
